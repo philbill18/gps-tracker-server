5bee81d7c702467de194af9f45d6267ee735e287
philippo-source <nollphil9@gmail.com>
2023-11-11 17:01:55 +0100

    added json parser

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
 });
 app.post('/gps-data', (req, res) => {
-    console.log(req);
+    //console.log(req);
     console.log(req.body);
-    res.send(req.body);
+    const gpsData = req.body;
+    res.send(gpsData);
   });
 
 app.listen(port, () => {
