bc3ce1d15badbe086b8017820efc0822ca36de1c
philippo-source <nollphil9@gmail.com>
2023-11-11 16:57:03 +0100

    up

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
 });
+app.post('/gps-data', (req, res) => {
+    console.log(req);
+    console.log(req.body);
+    res.send(req.body);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
