1059d814d32c418ebc640daeaa6cb6a91ea3840c
philippo-source <nollphil9@gmail.com>
2023-11-11 17:21:04 +0100

    added cors for client url

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: 'https://philbill18.github.io', // Replace with the actual URL of your client
+    optionsSuccessStatus: 200,
+  };
+  
+  app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
